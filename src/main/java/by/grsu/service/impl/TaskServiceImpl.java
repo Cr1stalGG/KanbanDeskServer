736780c5293e102ed6d495be5d231ef3a27399bd
Alexander Savitskiy <64911519+Cr1stalGG@users.noreply.github.com>
2024-05-23 01:40:21 +0300

    debug db

--- src/main/java/by/grsu/service/impl/TaskServiceImpl.java
+++ src/main/java/by/grsu/service/impl/TaskServiceImpl.java
@@ -42,7 +42,5 @@ public class TaskServiceImpl implements TaskService {
 
         Panel fromPanel = panelRepository.findById(moveTaskDto.getFromPanelId());
         Panel toPanel = panelRepository.findById(moveTaskDto.getToPanelId());
-
-        fromPanel.
     }
 }
