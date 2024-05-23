4be2ccee42705b6cee0d0f97b56573992b861147
Alexander Savitskiy <64911519+Cr1stalGG@users.noreply.github.com>
2024-05-23 03:04:48 +0300

    updateble

--- src/main/java/by/grsu/dto/account/AccountBaseDto.java
+++ src/main/java/by/grsu/dto/account/AccountBaseDto.java
@@ -6,7 +6,7 @@ import lombok.Data;
 @Data
 @Builder
 public class AccountBaseDto {
-    private long uuid;
+    private long uuid;//asd
     private String firstname;
     private String lastname;
 }
